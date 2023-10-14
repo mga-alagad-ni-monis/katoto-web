efb7659a9f31eec2db99b0dab188798d5d1508c4
Alvin Panerio <panerio.alvin18@gmail.com>
2023-10-15 00:05:20 +0800

    [FIX] Fix view campaigns layout

--- client/src/pages/CampaignView.js
+++ client/src/pages/CampaignView.js
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "../api/axios";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { HiArrowLongRight } from "react-icons/hi2";
 
 import { Markup } from "interweave";
+
+import { toHeaderCase } from "js-convert-case";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -18,6 +22,11 @@ function CampaignView({ token, auth, toast }) {
   const [campaigns, setCampaigns] = useState([]);
 
   const [campaignInfo, setCampaignInfo] = useState("");
+  const [search, setSearch] = useState("");
+  const [announcementFilter, setAnnouncementFilter] = useState("");
+  const [eventFilter, setEventFilter] = useState("");
+  const [webinarFilter, setWebinarFilter] = useState("");
+  const [campaignTitle, setCampaignTitle] = useState("");
 
   const [isShow, setIsShow] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
@@ -62,11 +71,77 @@ function CampaignView({ token, auth, toast }) {
           },
         })
         .then((res) => {
-          setCampaigns(res?.data?.campaigns);
+          const newCampaigns = res?.data?.campaigns?.map((i) => {
+            i["effectivityDate"] = convertDate(i["effectivityDate"])[1];
+            return i;
+          });
+
+          setCampaigns(newCampaigns);
         });
     } catch (err) {
       toast.error("Error");
     }
+  };
+
+  const convertDate = (date) => {
+    const formattedDate = new Date(date);
+
+    const convertedDateTime = formattedDate.toLocaleString("en-PH", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      timeZone: "Asia/Singapore",
+    });
+
+    const convertedDate = formattedDate.toLocaleString("en-PH", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      timeZone: "Asia/Singapore",
+    });
+
+    const convertedTime = formattedDate.toLocaleString("en-PH", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      timeZone: "Asia/Singapore",
+    });
+
+    return [convertedDateTime, convertedDate, convertedTime];
+  };
+
+  const filteredCampaigns = () => {
+    const newCampaigns = campaigns
+      ?.filter((i) => {
+        if (search?.toLowerCase().trim()) {
+          return (
+            i?.title.toLowerCase().includes(search.toLowerCase()) ||
+            i?.effectivityDate.toLowerCase().includes(search.toLowerCase()) ||
+            i?.description.toLowerCase().includes(search.toLowerCase()) ||
+            i?.campaignType.toLowerCase().includes(search.toLowerCase())
+          );
+        } else {
+          return i;
+        }
+      })
+      ?.filter((i) => {
+        if (announcementFilter?.toLowerCase().trim()) {
+          return i?.campaignType
+            .toLowerCase()
+            .includes(announcementFilter?.toLowerCase().trim());
+        } else if (eventFilter?.toLowerCase().trim()) {
+          return i?.campaignType
+            .toLowerCase()
+            .includes(eventFilter?.toLowerCase().trim());
+        } else {
+          return i;
+        }
+      });
+
+    return newCampaigns;
   };
 
   return (
@@ -74,60 +149,39 @@ function CampaignView({ token, auth, toast }) {
       {isLoading ? (
         <Loading />
       ) : (
-        <div className="bg-[--light-brown] min-h-screen pt-24">
-          <div className="flex flex-col px-52">
+        <div className="bg-[--light-brown] pt-2 min-h-screen pb-32">
+          <div className="flex flex-col">
             {isShow ? (
-              <div className="pt-20">
-                <div className="w-full pb-12">
-                  <button
-                    className="bg-black rounded-lg text-sm font-bold text-[--light-brown] py-2 pr-5 pl-3 flex gap-2 items-center justify-center 
-          border border-2 border-black hover:border-black hover:border-2 hover:bg-transparent hover:text-black transition-all duration-300"
-                    onClick={() => {
-                      setIsShow(false);
-                    }}
-                  >
-                    <IoChevronBackOutline size={16} />
-                    Back
-                  </button>
+              <div className="pt-20 px-60">
+                <div className="w-full pb-12 flex gap-3">
+                  <span>
+                    <button
+                      onClick={() => {
+                        setIsShow(false);
+                      }}
+                      className="text-[--dark-green] hover:underline"
+                    >
+                      Campaigns
+                    </button>{" "}
+                    / {toHeaderCase(campaignTitle)}
+                  </span>
                 </div>
                 <Markup content={campaignInfo} />
               </div>
             ) : (
               <>
-                <p className="mt-16 flex w-full text-3xl font-extrabold mb-8 justify-center">
-                  Campaigns
-                </p>
-                <div className="flex w-full gap-5">
-                  <div className="w-1/3 flex flex-col gap-5 max-screen">
-                    <p className="font-extrabold">Upcoming Campaigns</p>
-                    {campaigns?.map((i, k) => {
-                      return (
-                        <div
-                          key={k}
-                          className="bg-[--light-green] rounded-xl py-4 px-6 shadow-lg"
-                        >
-                          <p className="bg-[--dark-green] text-[--light-green] font-semibold rounded-lg w-max p-2 py-1 text-xs">
-                            {i?.campaignType}
-                          </p>
-                          <div className="mt-3 font-extrabold">{i?.title}</div>
-                          <div className="flex justify-between">
-                            <div className="mt-3 font-semibold text-sm">
-                              Until: {i?.effectivityDate}
-                            </div>
-                            <button
-                              className="bg-[--dark-green] rounded-full font-semibold text-sm text-[--light-green] px-3 py-1 border-2 border-[--dark-green] 
-                    hover:bg-[--light-green] hover:text-[--dark-green] transition-all duration-300"
-                              onClick={() => {
-                                setIsShow(true);
-                                setCampaignInfo(i?.campaignInfo);
-                              }}
-                            >
-                              See More
-                            </button>
-                          </div>
-                        </div>
-                      );
-                    })}
+                <div className="mt-16 h-96 px-60 flex w-full mb-8 items-center bg-[url('./assets/campaign-bg-sample.jpg')] bg-center bg-no-repeat bg-cover">
+                  <div className="items-start w-3/5 h-fit flex flex-col glass p-10 gap-5 text-[--light-brown]">
+                    <p className="text-3xl font-extrabold">
+                      Join us on our campaigns!
+                    </p>
+                    <p>
+                      Come and be a part of our inclusive events, where we will
+                      explore the importance of mental well-being, share
+                      strategies for coping with everyday challenges, breaking
+                      the stigma and create a supportive community dedicated to
+                      nurturing positive mental health for all.
+                    </p>
                   </div>
                   <Swiper
                     pagination={{
@@ -136,7 +190,7 @@ function CampaignView({ token, auth, toast }) {
                     modules={[Pagination]}
                     className="mySwiper w-1/3 rounded-xl -mt-[60px]"
                   >
-                    {campaigns?.map((i, k) => {
+                    {/* {campaigns?.map((i, k) => {
                       return (
                         <SwiperSlide
                           key={k}
@@ -154,24 +208,161 @@ function CampaignView({ token, auth, toast }) {
                           </div>
                         </SwiperSlide>
                       );
-                    })}
+                    })} */}
                   </Swiper>
-                  <div className="relative mt-36">
-                    <img
-                      src={katotoWatch}
-                      alt="katoto"
-                      className="h-[270px] absolute -top-[250px] right-1/2 translate-x-1/2"
-                    />
-                    <div className="h-max w-[350px] bg-[--light-brown] rounded-2xl border-2 border-black/10 shadow-lg pt-10 pb-5 px-5">
-                      <p className="text-2xl font-extrabold flex justify-center mb-5">
-                        Quote of the Day
+                </div>
+                <div className="flex w-full gap-10 px-60">
+                  <div className="w-[300px] flex flex-col gap-5">
+                    <p className="font-extrabold">Refine Results</p>
+                    <p>
+                      Filters and preferences automatically apply to results
+                      when selected.
+                    </p>
+                    <div className="mt-3">
+                      <p className="mb-3 font-bold text-xs">
+                        What are you looking for?
                       </p>
-                      <div className="bg-[--light-green] p-5 rounded-lg text-center text-base">
-                        <p>{quote?.quote}</p>
-                        <p className="font-semibold mt-2 italic">
-                          {quote?.author}
-                        </p>
+                      <input
+                        type="text"
+                        placeholder="Search..."
+                        className=" py-2 px-5 bg-black/10 rounded-lg text-sm focus:outline-black/50 placeholder-black/30 font-semibold"
+                        value={search}
+                        onChange={(e) => {
+                          setSearch(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <p className="mb-3 font-bold text-xs">Preferences</p>
+                      <div className="flex flex-col gap-3">
+                        <div className="flex gap-3 items-center">
+                          <input
+                            id="checkbox-1"
+                            className="text-[--light-brown] w-5 h-5 ease-soft text-xs rounded-lg checked:bg-[--dark-green] checked:from-gray-900 
+   checked:to-slate-800 after:text-xxs after:font-awesome after:duration-250 after:ease-soft-in-out duration-250 relative 
+   float-left cursor-pointer appearance-none border border-solid border-2  border-[--dark-green] bg-[--light-green] 
+   bg-contain bg-center bg-no-repeat align-top transition-all after:absolute after:flex after:h-full after:w-full 
+   after:items-center after:justify-center after:text-white after:opacity-0 after:transition-all after:content-['✔'] 
+   checked:border-0 checked:border-transparent checked:bg-[--dark-green] checked:after:opacity-100 mr-1"
+                            type="checkbox"
+                            style={{
+                              fontFamily: "FontAwesome",
+                            }}
+                            onChange={() => {
+                              setAnnouncementFilter(
+                                announcementFilter ? "" : "announcement"
+                              );
+                            }}
+                            checked={announcementFilter ? true : false}
+                          />
+                          <label htmlFor="checkbox-1">Announcement</label>
+                        </div>
+                        <div className="flex gap-3 items-center">
+                          <input
+                            id="checkbox-2"
+                            className="text-[--light-brown] w-5 h-5 ease-soft text-xs rounded-lg checked:bg-[--dark-green] checked:from-gray-900 
+   checked:to-slate-800 after:text-xxs after:font-awesome after:duration-250 after:ease-soft-in-out duration-250 relative 
+   float-left cursor-pointer appearance-none border border-solid border-2  border-[--dark-green] bg-[--light-green] 
+   bg-contain bg-center bg-no-repeat align-top transition-all after:absolute after:flex after:h-full after:w-full 
+   after:items-center after:justify-center after:text-white after:opacity-0 after:transition-all after:content-['✔'] 
+   checked:border-0 checked:border-transparent checked:bg-[--dark-green] checked:after:opacity-100 mr-1"
+                            type="checkbox"
+                            style={{
+                              fontFamily: "FontAwesome",
+                            }}
+                            onChange={() => {
+                              setEventFilter(eventFilter ? "" : "event");
+                            }}
+                            checked={eventFilter ? true : false}
+                          />
+                          <label htmlFor="checkbox-2">Event</label>
+                        </div>
+                        <div className="flex gap-3 items-center">
+                          <input
+                            id="checkbox-3"
+                            className="text-[--light-brown] w-5 h-5 ease-soft text-xs rounded-lg checked:bg-[--dark-green] checked:from-gray-900 
+   checked:to-slate-800 after:text-xxs after:font-awesome after:duration-250 after:ease-soft-in-out duration-250 relative 
+   float-left cursor-pointer appearance-none border border-solid border-2  border-[--dark-green] bg-[--light-green] 
+   bg-contain bg-center bg-no-repeat align-top transition-all after:absolute after:flex after:h-full after:w-full 
+   after:items-center after:justify-center after:text-white after:opacity-0 after:transition-all after:content-['✔'] 
+   checked:border-0 checked:border-transparent checked:bg-[--dark-green] checked:after:opacity-100 mr-1"
+                            type="checkbox"
+                            style={{
+                              fontFamily: "FontAwesome",
+                            }}
+                            onChange={() => {
+                              setWebinarFilter(webinarFilter ? "" : "webinar");
+                            }}
+                            checked={webinarFilter ? true : false}
+                          />
+                          <label htmlFor="checkbox-3">Webinar</label>
+                        </div>
                       </div>
+                    </div>
+                  </div>
+                  <div className="w-4/5 flex flex-col gap-8 max-screen">
+                    <p className="font-extrabold">Upcoming Campaigns</p>
+                    <div className="flex gap-5">
+                      {filteredCampaigns()?.map((i, k) => {
+                        return (
+                          <div
+                            className="w-1/3 flex flex-col border-[1px] border-black/20 rounded-xl"
+                            key={k}
+                          >
+                            <div className="relative">
+                              {i?.imageHeader ? (
+                                <img
+                                  src={i?.imageHeader}
+                                  className="w-full rounded-t-xl h-[200px] cursor-pointer"
+                                  onClick={() => {
+                                    setIsShow(true);
+                                    setCampaignInfo(i?.campaignInfo);
+                                    setCampaignTitle(i?.title);
+                                  }}
+                                ></img>
+                              ) : (
+                                <div
+                                  onClick={() => {
+                                    setIsShow(true);
+                                    setCampaignInfo(i?.campaignInfo);
+                                    setCampaignTitle(i?.title);
+                                  }}
+                                  className="w-full bg-gradient-to-t from-[--light-green] to-[#1cd8d2] h-[200px] rounded-t-xl cursor-pointer"
+                                ></div>
+                              )}
+                              <div className="bg-[--dark-green] rounded-full py-1 px-2 w-fit text-sm text-[--light-brown] absolute right-4 top-4">
+                                {toHeaderCase(i?.campaignType)}
+                              </div>
+                            </div>
+
+                            <div className="w-full p-5 flex flex-col gap-5">
+                              <p
+                                className="font-semibold text-[--dark-green] cursor-pointer duration-300 text-2xl"
+                                onClick={() => {
+                                  setIsShow(true);
+                                  setCampaignInfo(i?.campaignInfo);
+                                  setCampaignTitle(i?.title);
+                                }}
+                              >
+                                {toHeaderCase(i?.title)}
+                              </p>
+                              <p className="">Until: {i?.effectivityDate}</p>
+                              <p className="truncate">{i?.description}</p>
+                              <button
+                                className="h-fit w-fit bg-[--dark-green] rounded-lg text-sm font-bold text-[--light-brown] py-2 pr-5 pl-3 flex gap-2 items-center justify-center 
+          border border-2 border-[--dark-green] hover:border-[--dark-green] hover:border-2 hover:bg-transparent hover:text-[--dark-green] transition-all duration-300"
+                                onClick={() => {
+                                  setIsShow(true);
+                                  setCampaignInfo(i?.campaignInfo);
+                                  setCampaignTitle(i?.title);
+                                }}
+                              >
+                                Learn More <HiArrowLongRight />
+                              </button>
+                            </div>
+                          </div>
+                        );
+                      })}
                     </div>
                   </div>
                 </div>
